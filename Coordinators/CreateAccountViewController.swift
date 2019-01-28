f5dd465d593d8a7b14deb44fdbab526397adef94
Vlastimir <vlastimirsd@gmail.com>
2019-01-28 08:32:38 +0100

    finished adding a few more views

---
+++ Coordinators/CreateAccountViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreateAccountViewController.swift
+//  Coordinators
+//
+//  Created by Vlastimir Radojevic on 1/28/19.
+//  Copyright © 2019 Vlastimir Radojevic. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController, Storyboarded {
+    
+    weak var coordinator: MainCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Coordinators/ViewController.swift
+++ Coordinators/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController, Storyboarded {
+    
+    weak var coordinator: MainCoordinator?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buyTapped(_ sender: UIButton) {
+        coordinator?.buySubscription()
+    }
+    
+    @IBAction func createAccountTapped(_ sender: UIButton) {
+        coordinator?.createAccount()
+    }
 }
 
